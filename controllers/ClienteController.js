61a381dc27acdf935e5ccf075aef9612d2d703d0
vinicius.souza <viniciuspodi@gmail.com>
2019-05-03 19:57:44 -0400

    Adicionando log em cliente controller

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -76,7 +76,8 @@ class ClienteController{
             let resultado = (await Cliente.findByIdAndUpdate(req.body._id, req.body))
             res.status(200).send(resultado)
         } catch(error){
-            res.status(500).send("Erro ao editar convidado")
+            console.log("ERRO AO EDITAR CONVIDADO: " + error)
+            res.status(500).send("Erro ao editar cliente")
         }
     }
 
